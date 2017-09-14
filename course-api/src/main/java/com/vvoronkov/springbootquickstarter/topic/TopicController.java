009095d6ec05da8eb5da515c9ef6c9bd1d315c4c
Voronkov <VVoronkov@kiev.luxoft.com>
2017-09-14 16:43:00 +0300

    Added TopicService and TopicController.

--- course-api/src/main/java/com/vvoronkov/springbootquickstarter/topic/TopicController.java
+++ course-api/src/main/java/com/vvoronkov/springbootquickstarter/topic/TopicController.java
@@ -1,20 +1,38 @@
 package com.vvoronkov.springbootquickstarter.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "Javascript", "Javascript Description")
-        );
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{topicId}")
+    public Topic getTopic(@PathVariable("topicId") String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String topicId) {
+        topicService.updateTopic(topic, topicId);
+    }
+
+    @RequestMapping(value = "/topics/{topicId}", method = RequestMethod.DELETE)
+    public void deleteTopic(@PathVariable("topicId") String id) {
+        topicService.deleteTopic(id);
     }
 }
---
+++ course-api/src/main/java/com/vvoronkov/springbootquickstarter/topic/TopicService.java
@@ -0,0 +1,42 @@
+package com.vvoronkov.springbootquickstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("java", "Core Java", "Core Java Description"),
+            new Topic("javascript", "Javascript", "Javascript Description")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String topicId) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(topicId)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
